8f7dbc7305755aaa236aebd094dd83e70eb30b53
vishal tyagi <vishaltyagi.gh@gmail.com>
2021-12-01 10:42:07 +0530

    Add facadew

---
+++ src/Facades/Honchos.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Honchos\Ratelimiter\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \VendorName\Skeleton\Skeleton
+ */
+class Honchos extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'Honchos';
+    }
+}
